ebe0b06ca0f13073d410b15868c66e52f9ce5fd6
pradtf <34616154+pradtf@users.noreply.github.com>
2020-12-20 13:58:15 -0800

    bscrape without if statement
    it simply prints out the ticker and the number of occurences

--- Vegan Investors/bscrape.sh
+++ Vegan Investors/bscrape.sh
@@ -4,15 +4,17 @@
 # usage: just run it (jri)
 
 # list of stock tickers (should get these from a csv file though)
-tickers=("HTG.L" "AMPE" "TSN" "BYND")
+tickers=("TSN" "HTG.L" "AMPE" "TSN" "BYND" "VRYYF" "EATS.CN")
+
+# list of yes keywords
+yy="vegan|plant-based|cruelty-free|dairy-free|egg-free"
 
 template="https://finance.yahoo.com/quote/"
 
 for ticker in ${tickers[@]}; do
     url=$template$ticker"/profile"
-    echo $url
-    curl -s $url | grep -ciE "plant-based" 
+    isthere=`curl -s $url | grep -ciE $yy`
+    echo "$ticker : $isthere occurences"
 done
 
 exit
-
