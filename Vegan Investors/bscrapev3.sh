2fbacabef9fbd3564542f835e66599edd9b62991
pradtf <34616154+pradtf@users.noreply.github.com>
2020-12-20 20:46:20 -0800

    uses brittany's idea description(.*)corporate
    tries to pull out code btn description .. corporate so there is less to grep.
    unfortunately, we need to do this with sed before grepping and sed seems to be slowing things down a bit.
    however, it was a really good idea in principle, but is more suited to a proper webscraper than bash (which being an interpreted language is pretty slow anyway).

---
+++ Vegan Investors/bscrapev3.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# checks if keywords appear in a webpage
+# usage: just run it (jri)
+
+FILE=$1
+
+ar=()
+while read -ra a b; do
+    ar+=("$a")
+done < "$FILE"
+tickers="${ar[@]:1}"
+
+# list of yes keywords
+yy="vegan|plant-based|cruelty-free|dairy-free|egg-free"
+
+template="https://finance.yahoo.com/quote/"
+
+for ticker in ${tickers[@]}; do
+    echo $ticker
+    url=$template$ticker"/profile"
+    isthere=`curl -s $url | sed -e "s/Description(.*)Corporate Governance/\0/" | grep -ciE $yy`
+    if [ $isthere -gt 0 ]
+    then
+       echo "$ticker : $isthere occurences"
+    fi
+done
+
+exit
